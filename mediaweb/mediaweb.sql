864cc1e0030f7eaa399e961ace0a2ae768c666e0
Dylan Rakotoarivelo <dylanrakotoarivelo@gmail.com>
2020-03-22 23:37:47 +0100

    Rendu Final

--- mediaweb/mediaweb.sql
+++ mediaweb/mediaweb.sql
@@ -1,33 +1,87 @@
+-- phpMyAdmin SQL Dump
+-- version 4.8.5
+-- https://www.phpmyadmin.net/
+--
+-- Hôte : 127.0.0.1:3306
+-- Généré le :  Dim 22 mars 2020 à 22:36
+-- Version du serveur :  5.7.26
+-- Version de PHP :  7.2.18
+
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
 SET time_zone = "+00:00";
 
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de données :  `mediaweb`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `documents`
+--
+
+DROP TABLE IF EXISTS `documents`;
 CREATE TABLE IF NOT EXISTS `documents` (
   `idDoc` int(11) NOT NULL AUTO_INCREMENT,
-  `title` varchar(150) NOT NULL,
-  `author` varchar(60) NOT NULL,
+  `title` varchar(150) COLLATE utf8_bin NOT NULL,
+  `author` varchar(60) COLLATE utf8_bin NOT NULL,
   `typeDoc` int(11) NOT NULL,
-  `login` varchar(20) DEFAULT NULL,
+  `login` varchar(20) COLLATE utf8_bin DEFAULT NULL,
   PRIMARY KEY (`idDoc`),
   KEY `FK_login` (`login`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin AUTO_INCREMENT=1;
+) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
 
-CREATE TABLE IF NOT EXISTS `users` (
-  `login` varchar(20) NOT NULL,
-  `password` varchar(20) NOT NULL,
-  `typeUser` varchar(20) NOT NULL,
-  PRIMARY KEY (`login`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin AUTO_INCREMENT=1;
-
-ALTER TABLE `documents`
-  ADD CONSTRAINT `FK_login` FOREIGN KEY (`login`) REFERENCES `users` (`login`) ON DELETE SET NULL ON UPDATE CASCADE;
-COMMIT;
+--
+-- Déchargement des données de la table `documents`
+--
 
 INSERT INTO `documents` (`idDoc`, `title`, `author`, `typeDoc`, `login`) VALUES
 (1, 'Tintin en Amérique', 'Georges Rémi', 1, NULL),
 (2, 'Pirates des Caraïbes', 'Gore Verbinski', 2, NULL),
-(3, 'Nyxia Tome 3', 'Tayc', 3, NULL),
+(3, 'Nyxia Tome 3', 'Tayc', 3, 'user'),
 (4, 'Naruto', 'Masashi Kishimoto', 1, NULL);
 
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `users`
+--
+
+DROP TABLE IF EXISTS `users`;
+CREATE TABLE IF NOT EXISTS `users` (
+  `login` varchar(20) COLLATE utf8_bin NOT NULL,
+  `password` varchar(20) COLLATE utf8_bin NOT NULL,
+  `typeUser` varchar(20) COLLATE utf8_bin NOT NULL,
+  PRIMARY KEY (`login`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+
+--
+-- Déchargement des données de la table `users`
+--
+
 INSERT INTO `users` (`login`, `password`, `typeUser`) VALUES
-('user', 'user', 'abonne'),
-('admin', 'admin', 'bibliothecaire');
+('admin', 'admin', 'bibliothecaire'),
+('user', 'user', 'abonne');
+
+--
+-- Contraintes pour les tables déchargées
+--
+
+--
+-- Contraintes pour la table `documents`
+--
+ALTER TABLE `documents`
+  ADD CONSTRAINT `FK_login` FOREIGN KEY (`login`) REFERENCES `users` (`login`) ON DELETE SET NULL ON UPDATE CASCADE;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
